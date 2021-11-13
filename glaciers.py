9be4b2a09cfef03c980daba3828268a4bf39aeca
jiahui Li <415234364@qq.com>
2021-11-14 07:09:57 +0800

    implement constructor of glaciercollection

--- glaciers.py
+++ glaciers.py
@@ -1,18 +1,44 @@
+
 class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
-        raise NotImplementedError
+        self.glacier_id=glacier_id
+        self.name=name
+        self.unit=unit
+        self.lat=lat
+        self.lon=lon
+        self.code=code
 
-    def add_mass_balance_measurement(self, year, mass_balance):
+        
+
+    def add_mass_balance_measurement(self, year, mass_balance,partial_measurement):
         raise NotImplementedError
 
     def plot_mass_balance(self, output_path):
         raise NotImplementedError
 
         
+
+from pathlib import Path
+import csv
+
 class GlacierCollection:
 
     def __init__(self, file_path):
-        raise NotImplementedError
+        self.file_path=file_path
+        self.collectionObject=[]
+        with file_path.open() as file:          
+             reader = csv.reader(file)
+             next(reader)
+            #  lines=len(list(reader))
+             for row in reader:
+                Glacier_object=Glacier(row[2],row[1],row[0],row[5],row[6],row[14])
+                self.collectionObject.append(Glacier_object)
+                
+        
+        
+
+                
+        
 
     def read_mass_balance_data(self, file_path):
         raise NotImplementedError
@@ -34,3 +60,11 @@ class GlacierCollection:
 
     def plot_extremes(self, output_path):
         raise NotImplementedError
+
+
+def main():
+    file_path = Path("sheet-A.csv")
+    collection = GlacierCollection(file_path)
+
+if __name__ == "__main__":
+    main()
