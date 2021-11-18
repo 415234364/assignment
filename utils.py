cb614944ca407d3e2ac7664b121776dd9bb19583
jiahui Li <415234364@qq.com>
2021-11-18 21:04:39 +0800

    remove numpy

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 import math
 def validation(latitude,lontitude):
     if latitude>90 or latitude<-90 or lontitude>180 or lontitude<-180:
@@ -10,5 +10,6 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     Latitude and longitude for each point are given in degrees.
     """
     if validation(lat1,lon1) and validation(lat2,lon2):
-        distance=2*6371*np.arcsin(math.sqrt(math.sin((lat2-lat1)/2)**2+math.cos(lat1)*math.cos(lat2)*math.sin((lon2-lon1)/2)**2))
+        distance=2*6371*math.asin(math.sqrt(math.sin((lat2-lat1)/2)**2+math.cos(lat1)*math.cos(lat2)*math.sin((lon2-lon1)/2)**2))
+        
     return distance
